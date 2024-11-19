daf9d97839b878438f1efd2a151bae4db528d8ed
VaradNerlekarUMD <nerlekar@umd.edu>
2024-11-19 17:50:30 -0500

    fix: header correction for galactic

--- lecture12/p_controller_demo/include/p_controller_demo/p_controller_demo_node.hpp
+++ lecture12/p_controller_demo/include/p_controller_demo/p_controller_demo_node.hpp
@@ -4,7 +4,7 @@
 #include "geometry_msgs/msg/twist.hpp"
 #include "nav_msgs/msg/odometry.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include "color_utils.hpp"
 
 class ProportionalControllerDemoNode : public rclcpp::Node {
